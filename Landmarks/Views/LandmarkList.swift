62754754130230757a2f2d1f15925ca4978cf040
KyungsooLee <jmtkd9196@gmail.com>
2022-08-26 04:21:57 +0900

    Implement MapView, CircleImage, LandmarkList, LandmarkDetail

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -12,7 +12,7 @@ struct LandmarkList: View {
         NavigationView {
             List(landmarks) { landmark in
                 NavigationLink {
-                    LandmarkDetail()
+                    LandmarkDetail(landmark: landmark)
                 } label: {
                     LandmarkRow(landmark: landmark)
                 }
@@ -24,6 +24,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
+        ForEach(["iPhone SE (3rd generation)", "iPhone XS Max", "iPad Pro (12.9-inch) (5th generation)"], id: \.self) { deviceName in
         LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: deviceName))
+                            .previewDisplayName(deviceName)
+        }
     }
 }
