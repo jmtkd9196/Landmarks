db7aa60e6eb76b1443f6a25ec6d36f4246bd1c11
KyungsooLee <jmtkd9196@gmail.com>
2022-08-31 03:22:53 +0900

    Implement ProfileHost

--- Landmarks/Views/Profile/ProfileHost.swift
+++ Landmarks/Views/Profile/ProfileHost.swift
@@ -14,6 +14,12 @@ struct ProfileHost: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
                 Spacer()
                 EditButton()
             }
@@ -21,6 +27,12 @@ struct ProfileHost: View {
                 ProfileSummary(profile: modelData.profile)
             } else {
                 ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
             }
         }
         .padding()
